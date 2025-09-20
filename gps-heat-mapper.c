ea02571f02cac0113e983a86c9606d37e7e42f63
baikman <baikman@cedarville.edu>
2025-09-20 16:56:11 -0400

    updated gitignore lol

--- gps-heat-mapper.c
+++ gps-heat-mapper.c
@@ -5,19 +5,15 @@
 #include "minmea.h"
 
 // I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
 #define I2C_SDA 8
 #define I2C_SCL 9
 
+
 // UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
 #define UART_ID uart1
 #define BAUD_RATE 115200
 
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define UART_TX_PIN 4
 #define UART_RX_PIN 5
 
@@ -26,15 +22,13 @@ char line[MINMEA_MAX_SENTENCE_LENGTH];
 int main(){
     stdio_init_all();
 
-    // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
-
+    
     uart_init(UART_ID, 9600);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
@@ -57,6 +51,7 @@ int main(){
                                minmea_tocoord(&frame.longitude));
                     }
                 }
+            
             } else if (idx < sizeof(line) - 1) {
                 line[idx++] = c;
             }
